8f38a70465a5bf77c9a9b96d2e6b453cea8d9153
3Nk <72727335+AnanasikD@users.noreply.github.com>
2024-03-01 11:45:35 +0300

    add files and make animation for player

--- Divine Rebellion/Player/player.gd
+++ Divine Rebellion/Player/player.gd
@@ -1,14 +1,29 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
-@onready var animations = $AnimationPlayer
+@export var speed = 30
+@onready var animations = $AnimatedSprite2D
+var direction = "down"
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = moveDirection * speed
+	velocity = moveDirection
+	if velocity.x != 0 and velocity.y != 0:
+		velocity *= sqrt(speed ** 2)
+	else:
+		velocity *= speed
 
-
-	
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.play("idle_" + direction)
+		animations.stop()
+	else:
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+		elif velocity.y < 0: direction = "up"
+		elif velocity.y > 0: direction = "down"
+		animations.play("walk_" + direction)
