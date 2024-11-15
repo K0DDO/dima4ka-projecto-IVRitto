33f1a80cfeb51605cbdf1dfce9621557384a8419
Koddo <72727335+K0DDO@users.noreply.github.com>
2024-11-15 05:59:18 +0300

    ласт коммит

--- Divine Rebellion/Scripts/Game/changeScenes/toPlayerHouse2.gd
+++ Divine Rebellion/Scripts/Game/changeScenes/toPlayerHouse2.gd
@@ -3,6 +3,7 @@ extends Area2D
 var in_zone = false
 @onready var animation = $"../../../Player/CanvasLayer/ColorRect/AnimationPlayer"
 @onready var tile_map = $"../../.."
+var pressed = false
 
 func _on_body_entered(body):
 	if body.has_method("player"):
@@ -14,7 +15,8 @@ func _on_body_exited(body):
 
 func _input(_event):
 	if in_zone:
-		if Input.is_action_just_pressed("rightClick"):
+		if Input.is_action_just_pressed("rightClick") and !pressed:
+			pressed = true
 			TileMapSave.save_tilemap(tile_map)
 			Global.entry_point = Global.EntryPoint.DOOR
 			animation.play("fade_out")
---
+++ final_code.md
@@ -0,0 +1,1 @@
+https://disk.yandex.ru/i/xWYpw2YkKJWWvw
